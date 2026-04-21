1eaab48e4c02aca0df6a513bf89c19908f12cda8
Jilani Naouali <naoualijilani133@gmail.com>
2026-04-21 20:45:58 +0100

    Refactor file identifier to allow custom signatures

--- file_identifier.py
+++ file_identifier.py
@@ -2,6 +2,7 @@ import sys
 
 SIGNATURES = [
     # (name, extension, magic_bytes)
+    #you can add any SIGNATURES you want here
     ("JPEG Image",      ".jpg",  [0xFF, 0xD8, 0xFF]),
     ("PNG Image",       ".png",  [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A]),
     ("PDF Document",    ".pdf",  [0x25, 0x50, 0x44, 0x46]),
